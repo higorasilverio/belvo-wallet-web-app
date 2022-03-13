6157e8efb527eadd338aab59cf625a210a9d7b6e
higorasilverio <higorasilverio@outlook.com>
2022-03-13 13:42:34 -0300

    ✨ (useWindowDimensions): hook to get window width added

---
+++ src/hooks/useWindowDimensions.js
@@ -0,0 +1,26 @@
+import { useState, useEffect, useCallback } from "react";
+
+export const useWindowDimensions = () => {
+  const getWindowDimensions = useCallback(() => {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+      width,
+      height,
+    };
+  }, []);
+
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowDimensions(getWindowDimensions());
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [getWindowDimensions]);
+
+  return windowDimensions;
+};
